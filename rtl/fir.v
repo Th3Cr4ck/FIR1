ade44f6bbba106fc29aff95b531fa209ca31dbc0
Isaac Hernandez <isaac.ghc02@gmail.com>
2026-05-06 14:34:33 -0600

    Removed unused rtl modules

--- rtl/fir.v
+++ rtl/fir.v
@@ -1,32 +1,21 @@
 module fir #(
     parameter QI = 1,
     parameter QF = 14,
-    parameter ORDER = 3
+    parameter TAPS = 4
 ) (
     input clk,
     input rst_n,
+    input signed [(QI+QF-1):0] coeff[TAPS-1:0],
     input signed [(QI+QF-1):0] x_in,
     output reg signed [(QI+QF-1):0] y_out
 );
 
   localparam DATA_WIDTH = QI + QF;
   localparam PROD_WIDTH = DATA_WIDTH * 2;
-  localparam TAPS = ORDER + 1;
 
   // Bits extra para acumulación
   localparam ACC_EXTRA = $clog2(TAPS);
   localparam ACC_WIDTH = PROD_WIDTH + ACC_EXTRA;  // Ancho del acumulador
-
-  // Coeficientes
-  wire signed [(DATA_WIDTH-1):0] coeff[TAPS-1:0];
-
-  genvar i;
-  generate
-    for (i = 0; i < TAPS; i = i + 1) begin : gen_coeff
-      assign coeff[i] = 1 <<< (QF-1); //0.5
-    end
-  endgenerate
-
 
   // Registers
   reg signed [DATA_WIDTH-1:0] x_registers[TAPS-1:0];
@@ -36,16 +25,16 @@ module fir #(
   always @(posedge clk or negedge rst_n)
     if (!rst_n) begin
       for (k = 0; k < TAPS; k = k + 1) begin
-        x_registers[k]   <= 0;
+        x_registers[k] <= 0;
       end
       y_out <= 0;
 
     end else begin
       for (k = 1; k < TAPS; k = k + 1) begin
-        x_registers[k]   <= x_registers[k-1];
+        x_registers[k] <= x_registers[k-1];
       end
       x_registers[0] <= x_in;
-      y_out <= sum[TAPS-1][(2*QF) -: DATA_WIDTH];
+      y_out <= sum[TAPS-1][(2*QF)-:DATA_WIDTH];
     end
 
 
@@ -57,23 +46,9 @@ module fir #(
       assign mult_results[i] = x_registers[i] * coeff[i];
       assign sum[i] = sum[i-1] + mult_results[i];
     end
-
   endgenerate
 
   assign mult_results[0] = x_registers[0] * coeff[0];
   assign sum[0] = mult_results[0];
-  // assign tap_results_full[0] = mult_results[0];
 
 endmodule
-
-// sum_fxp #(
-//   .A_QI(2 * QI),
-//   .A_QF(2 * QF),
-//   .B_QI(QI),
-//   .B_QF(QF)
-// ) sum (
-//   .a  (mult_results[i]),
-//   .b  (tap_registers[i-1]),
-//   .sum(tap_results_full[i])
-// );
-
--- rtl/mult.v
+++
@@ -1,9 +0,0 @@
-module mult (
-input  [7:0] i_x,
-input  [7:0] i_y,
-output [7:0] o_z
-);
-
-assign o_z = i_x * i_y;
-
-endmodule
